fbe9fddf12476e7930381c54be6e329cf923c6ba
zhamalamazalieva <jamalamazalieva@gmail.com>
2020-11-09 11:48:50 +0100

    added a mobile version of the section 'transportation'

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 $('.transportation__slider').slick({
 	arrows: true,
 	prevArrow:'.prev__arrow',
-	nextArrow:'.next__arrow'
+	nextArrow:'.next__arrow',
+	responsive:[{
+		breakpoint:1024,
+		settings:{
+		  dots:true
+		}}]
 
 })
